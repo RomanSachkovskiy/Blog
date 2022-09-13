144ca6d5e341808f8c2ebf949b831a89c5b07b31
RomanSachkovskiy <84938597+RomanSachkovskiy@users.noreply.github.com>
2022-09-13 23:23:31 +0300

    Add files via upload

---
+++ SpringSt/src/test/java/com/example/SpringSt/SpringStApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
