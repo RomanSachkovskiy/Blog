df1594b0c7e6480389a6d2cbb01596027767a681
sachkovsky <roma.saxhkovskiy@ya.ru>
2022-09-22 19:24:31 +0300

    Добавлена форма для добавления статей

--- src/main/java/com/example/SpringSt/controllers/BlogController.java
+++ src/main/java/com/example/SpringSt/controllers/BlogController.java
@@ -14,9 +14,14 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
+    public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts",  posts);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
     }
 }
